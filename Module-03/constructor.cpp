af0d84ba44b8cf5aae1d8317baa12cf33fbcc0b7
Satyajit Ghosh <satyajit_jbl@yahoo.com>
2026-05-24 16:56:33 +0600

    readme

---
+++ Module-03/constructor.cpp
@@ -0,0 +1,20 @@
+`
+git clone https://github.com/satyajit-jbl/C-Learning.git
+`
+git status
+git pull origin main
+
+Office PC
+git pull origin main
+# edit code
+git add .
+git commit -m "message"
+git push origin main
+Home PC
+git pull origin main
+# edit code
+git add .
+git commit -m "message"
+git push origin main
+
+Always do git pull before starting work on another PC.
